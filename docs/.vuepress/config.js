859ff357825c6a6bec366fd81d84f34ef0b6ceea
Hongbusi <q951686662@qq.com>
2021-02-25 19:57:41 +0800

    init docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Hongbusi',
+  description: '桃李不言，下自成蹊！',
+  head: [
+    ['link', {rel: 'icon', href: '/logo.png'}]
+  ],
+  themeConfig: {
+    repo: 'Hongbusi',
+    locales: {
+
+    }
+  }
+}
