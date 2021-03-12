38dd22d8434d7c5b286c7930832ee990bc955b57
Hongbusi <q951686662@qq.com>
2021-03-12 11:46:40 +0800

    footer

--- docs/.vuepress/public/js/index.js
+++ docs/.vuepress/public/js/index.js
@@ -2,7 +2,7 @@ console.log('Hongbusi');
 
 window.onload = function () {
   const footer = document.getElementsByClassName('footer')[0];
-  const div = document.createElement('div');
-  div.innerHTML = '<a href="https://beian.miit.gov.cn" target="_blank">浙ICP备2020035758号-2</a>'
-  footer.appendChild(div);
+  const p = document.createElement('p');
+  p.innerHTML = '<a href="https://beian.miit.gov.cn" target="_blank">浙ICP备2020035758号-2</a>'
+  footer.appendChild(p);
 }
