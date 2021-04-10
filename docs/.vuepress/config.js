6c02efe42a7e10cad970545ccd8caf3ae9c80bdb
Hongbusi <q951686662@qq.com>
2021-04-11 00:57:38 +0800

    add vuepress-theme-hbs

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,6 +4,7 @@ module.exports = {
   head: [
     ['link', {rel: 'icon', href: 'logo.png'}]
   ],
+  theme: 'hbs',
   themeConfig: require('./config/themeConfig'),
   plugins: require('./config/plugins')
 }
