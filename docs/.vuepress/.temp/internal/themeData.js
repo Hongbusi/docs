838f011f900fa1c656f89cb344f4e4d33ce7dbc5
Hongbusi <q951686662@qq.com>
2021-07-06 12:36:05 +0800

    update

--- docs/.vuepress/.temp/internal/themeData.js
+++ docs/.vuepress/.temp/internal/themeData.js
@@ -27,16 +27,3 @@ export const themeData = {
   "backToHome": "Take me home",
   "openInNewWindow": "open in new window"
 }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updateThemeData) {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ themeData }) => {
-    __VUE_HMR_RUNTIME__.updateThemeData(themeData)
-  })
-}
