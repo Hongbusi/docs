4aad0f683de74d86a71e302898ae61d708ebaf94
Hongbusi <q951686662@qq.com>
2021-09-29 14:59:30 +0800

    update 2021-09-29 14:59:30

--- docs/.vuepress/components/DailyReading.vue
+++ docs/.vuepress/components/DailyReading.vue
@@ -1,6 +1,15 @@
 <template>
-  <h3 class="min-w-max">{{ frontmatter.title }}</h3>
-  <!-- <a v-for="(item, index) in frontmatter.data" :key="index" :href="item.link">{{ item.text }}</a> -->
+  <h3 class="text-center text-3xl font-medium border-b border-gray-200 pb-3">{{ frontmatter.title }}</h3>
+  <a
+    v-for="(item, index) in frontmatter.data"
+    :key="index" :href="item.link"
+    :title="item.title"
+    target="_blank"
+  >
+    <div class="p-3 hover:bg-gray-100">
+      <span class="mr-2">({{ item.date }})</span>{{ item.title }}
+    </div>
+  </a>
 </template>
 
 <script setup lang="ts">
