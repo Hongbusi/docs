b0d930ef6b00851ebd9ad050608a75c8b8972cd1
Hongbusi <q951686662@qq.com>
2022-02-09 11:52:22 +0800

    refactor: vuepress -> vitepress

---
+++ .vitepress/config.js
@@ -0,0 +1,44 @@
+/**
+ * @type {import('vitepress').UserConfig}
+ */
+
+const config = {
+  base: '/docs/',
+  srcDir: 'src',
+  title: 'Hongbusi',
+  description: 'The more you know, the more you do not know.',
+
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }]
+  ],
+
+  themeConfig: {
+    repo: 'Hongbusi/docs',
+    docsDir: 'docs',
+    docsBranch: 'master',
+    editLinks: true,
+    editLinkText: 'Edit this page on GitHub',
+    lastUpdated: 'Last Updated',
+
+    nav: [
+      { text: 'Frontend', link: '/frontend/' }
+    ],
+
+    sidebar: {
+      '/frontend/': getFrontendSidebar()
+    }
+  }
+};
+
+function getFrontendSidebar() {
+  return [
+    {
+      text: 'Webpack',
+      children: [
+        { text: 'Node Interface', link: '/frontend/webpack/node-interface' }
+      ]
+    }
+  ]
+}
+
+export default config;
