22cea470e4eaa8376f2642a2b721c42cbc4c300f
Hongbusi <q951686662@qq.com>
2022-04-07 22:59:53 +0800

    chore: update

--- .vitepress/nav/index.ts
+++ .vitepress/nav/index.ts
@@ -1,11 +1,5 @@
 const nav = [
-  {
-    text: 'Frontend',
-    items: [
-      { text: 'JavaScript', link: '/javascript/browser' },
-      { text: 'petite-vue', link: '/petite-vue/' }
-    ]
-  },
+  { text: 'Frontend', link: '/frontend/vue' },
   { text: 'Notes', link: '/notes/essays/2022' },
   { text: 'Interview', link: '/interview/' },
   { text: 'Bookmarks', link: '/bookmarks/' }
