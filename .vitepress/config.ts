4dd3d612637a174bb88089da289421f2153fcf16
Hongbusi <coderhbs@gmail.com>
2023-02-16 11:10:10 +0800

    feat: update

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -45,14 +45,54 @@ export default defineConfigWithTheme<Config>({
           { text: 'TypeScript', link: '/typescript/' },
         ]
       },
+      { text: 'Algorithm', link: '/algorithm/what-is-algorithm' },
       { text: 'Notes', link: '/notes/' },
-      { text: 'Rust', link: '/rust/' },
+      // { text: 'Rust', link: '/rust/' },
       { text: 'Interview', link: '/interview/css/box' },
       { text: 'Bookmarks', link: '/bookmarks/' }
     ],
 
     sidebar: {
       ...sidebar,
+      '/algorithm/': [
+        {
+          text: '入门篇',
+          items: [
+            { text: '什么是算法？', link: '/algorithm/what-is-algorithm', },
+            { text: '为什么要学习算法和数据结构？', link: '/algorithm/why-study-algorithms-and-data-structures' },
+            { text: '算法与数据结构的学习方法', link: '/algorithm/algorithms-and-data-structures-learning-methods' },
+            { text: '为什么要学习复杂度的分析方法？', link: '/algorithm/why-learn-complexity-analysis' }
+          ]
+        },
+        {
+          text: '基础篇',
+          items: [
+            { text: '递归程序设计技巧', link: '/algorithm/recursive-programming-techniques' },
+            { text: '顺序表与链表', link: '/algorithm/sequence-list-and-linked-list' },
+            { text: '栈与队列', link: '/algorithm/stacks-and-queues' },
+            { text: '二叉树', link: '/algorithm/binary-tree' },
+            { text: '堆与优先队列', link: '/algorithm/heap-and-priority-queue' },
+            { text: '排序与查找', link: '/algorithm/sort-and-find' },
+            { text: '二分算法', link: '/algorithm/binary-algorithm' },
+            { text: '平衡二叉排序树', link: '/algorithm/balanced-binary-sorting-tree' },
+            { text: '递归函数转非递归', link: '/algorithm/convert-recursive-function-to-non-recursive' }
+          ]
+        },
+        {
+          text: '高级篇',
+          items: [
+            { text: '森林与并查集', link: '/algorithm/forest-and-union' },
+            { text: '单调队列与单调栈', link: '/algorithm/monotonic-queue-and-monotonic-stack' },
+            { text: '回溯算法', link: '/algorithm/backtracking-algorithm' },
+            { text: '贪心算法', link: '/algorithm/greedy-algorithm' },
+            { text: '动态规划', link: '/algorithm/dynamic-programming' },
+            { text: '字符串匹配', link: '/algorithm/string-match' },
+            { text: '树状数组与线段树', link: '/algorithm/tree-arrays-and-segment-trees' },
+            { text: '树上的问题', link: '/algorithm/tree-problem' },
+            { text: '图', link: '/algorithm/picture' }
+          ]
+        }
+      ],
       '/rust/': [
         {
           text: 'Rust 程序设计语言',
