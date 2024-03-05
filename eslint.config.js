23fe573edc3370f2ef14189545458d27115c4e91
Hongbusi <coderhbs@gmail.com>
2024-03-05 22:43:37 +0800

    refactor: vitepress -> nextra

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,14 @@
-// eslint.config.js
-import antfu from '@antfu/eslint-config'
+const antfu = require('@antfu/eslint-config').default
 
-export default antfu()
+module.exports = antfu(
+  {
+    react: true,
+    rules: {
+      'node/prefer-global/process': 'off',
+      'react-refresh/only-export-components': 'off',
+    },
+  },
+  {
+    ignores: ['old-docs'],
+  },
+)
