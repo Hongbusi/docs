66ccf247bef199c44d452083955b7c6550a6d263
Hongbusi <coderhbs@gmail.com>
2025-06-18 17:52:02 +0800

    chore: 更新文档配置和依赖项，添加社交链接，删除不必要的文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,24 +9,44 @@ export default defineConfig({
   },
   integrations: [
     starlight({
-      title: '洪布斯的文档网站',
+      title: '洪布斯的文档',
+      description: '知道的越多，不知道的越多。',
+      head: [
+        {
+          tag: 'script',
+          attrs: {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-M0H8SV269C',
+          },
+        },
+        {
+          tag: 'script',
+          content: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M0H8SV269C');
+          `,
+        },
+      ],
       locales: {
         root: {
           label: '简体中文',
           lang: 'zh-CN',
         },
       },
-      social: {
-        github: 'https://github.com/Hongbusi',
-      },
+      social: [
+        { icon: 'twitter', label: 'Twitter', href: 'https://x.com/Hongbusi' },
+        { icon: 'github', label: 'GitHub', href: 'https://github.com/Hongbusi' },
+      ],
       sidebar: [
-        { label: '笔记', autogenerate: { directory: 'notes' } },
-        { label: '服务器', autogenerate: { directory: 'server' } },
-        { label: 'Nest', autogenerate: { directory: 'nestjs' } },
-        { label: 'MySQL', autogenerate: { directory: 'mysql' } },
-        { label: 'JavaScript', autogenerate: { directory: 'javascript' } },
-        { label: 'TypeScript', autogenerate: { directory: 'typescript' } },
-        { label: '面试', autogenerate: { directory: 'interview' } },
+        { label: '笔记', collapsed: true, autogenerate: { directory: 'notes' } },
+        { label: '服务器', collapsed: true, autogenerate: { directory: 'server' } },
+        { label: 'Nest', collapsed: true, autogenerate: { directory: 'nestjs' } },
+        { label: 'MySQL', collapsed: true, autogenerate: { directory: 'mysql' } },
+        { label: 'JavaScript', collapsed: true, autogenerate: { directory: 'javascript' } },
+        { label: 'TypeScript', collapsed: true, autogenerate: { directory: 'typescript' } },
+        { label: '面试', collapsed: true, autogenerate: { directory: 'interview' } },
       ],
       editLink: {
         baseUrl: 'https://github.com/Hongbusi/docs/edit/main/',
--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,4 @@ import antfu from '@antfu/eslint-config'
 
 export default antfu({
   astro: true,
-  ignores: [
-    '**/old',
-  ],
 })
